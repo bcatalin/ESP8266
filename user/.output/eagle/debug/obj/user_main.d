a846ca3945a315e4f8300134f6391ed1f31a52d5
Catalin <bcatalin@gmail.com>
2015-01-29 12:47:40 +0200

    First commit

---
+++ user/.output/eagle/debug/obj/user_main.d
@@ -0,0 +1,13 @@
+.output/eagle/debug/obj/user_main.o .output/eagle/debug/obj/user_main.d : user_main.c ../../include/osapi.h ../../include/string.h \
+ ../../include/_ansi.h ../../include/newlib.h ../../include/sys/config.h \
+ ../../include/machine/ieeefp.h ../../include/sys/reent.h \
+ ../../include/_ansi.h ../../include/sys/_types.h \
+ ../../include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../include/user_config.h ../../include/user_interface.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/ip_addr.h ../../include/queue.h \
+ ../../include/spi_flash.h ../../include/c_types.h ../../include/mem.h \
+ ../../include/ets_sys.h ../../include/gpio.h ../../include/ip_addr.h \
+ ../../include/espconn.h ../../include/os_type.h ../include/driver/uart.h \
+ ../include/driver/uart_register.h
